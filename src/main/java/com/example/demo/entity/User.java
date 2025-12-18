80a3ff99a21b0ca053d8eb7a4ff18e81218196ad
bala-amypo <bala@amypo.in>
2025-12-18 08:39:10 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,9 @@
+package.com.example.demo.entity;
+import jakarta.persistence.*;
+
+public class User(){
+    @id
+    private Long id;
+    private String resourceName;
+    private String resource
+}
