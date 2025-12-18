bcd56aefe8376246f81af8a5c7a07225d2869bd3
bala-amypo <bala@amypo.in>
2025-12-18 08:44:09 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,9 +1,61 @@
-package.com.example.demo.entity;
+package com.example.demo.entity;
 import jakarta.persistence.*;
 
-public class User(){
+public class User {
     @id
     private Long id;
     private String resourceName;
-    private String resource
+    private String resourceType;
+    private int capacity;
+    private String Location;
+    private localdate createdAt;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getResourceName() {
+        return resourceName;
+    }
+    public void setResourceName(String resourceName) {
+        this.resourceName = resourceName;
+    }
+    public String getResourceType() {
+        return resourceType;
+    }
+    public void setResourceType(String resourceType) {
+        this.resourceType = resourceType;
+    }
+    public int getCapacity() {
+        return capacity;
+    }
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+    public String getLocation() {
+        return Location;
+    }
+    public void setLocation(String location) {
+        Location = location;
+    }
+    public localdate getCreatedAt() {
+        return createdAt;
+    }
+    public User() {
+    }
+    public void setCreatedAt(localdate createdAt) {
+        this.createdAt = createdAt;
+    }
+    public User(Long id, String resourceName, String resourceType, int capacity, String location, localdate createdAt) {
+        this.id = id;
+        this.resourceName = resourceName;
+        this.resourceType = resourceType;
+        this.capacity = capacity;
+        Location = location;
+        this.createdAt = createdAt;
+    }
+
+
+    
 }
