dbf9f6fdbcdf989fcccae4bcfae4ee57c9fc4698
bala-amypo <bala@amypo.in>
2025-12-21 09:11:25 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ResourceAllocationServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ResourceAllocationServiceImpl.java
@@ -3,7 +3,7 @@ package com.example.demo.service;
 import com.example.demo.entity.ResourceAllocation;
 import java.util.List;
 
-public interface ResourceAllocationService {
+public class ResourceAllocationServiceImpl implements ResourceAllocationService {
     ResourceAllocation autoAllocate(Long requestId);
     ResourceAllocation getAllocation(Long id);
     List<ResourceAllocation> getAllAllocations();
