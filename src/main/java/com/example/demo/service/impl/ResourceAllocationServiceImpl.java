a673e8c65083f36a1e81c28d068d2241588cede5
bala-amypo <bala@amypo.in>
2025-12-21 09:26:25 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ResourceAllocationServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ResourceAllocationServiceImpl.java
@@ -1,10 +1,29 @@
-package com.example.demo.service;
+package com.example.demo.service.impl;
 
 import com.example.demo.entity.ResourceAllocation;
+import com.example.demo.service.ResourceAllocationService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class ResourceAllocationServiceImpl implements ResourceAllocationService {
-    ResourceAllocation autoAllocate(Long requestId);
-    ResourceAllocation getAllocation(Long id);
-    List<ResourceAllocation> getAllAllocations();
+
+    @Override
+    public ResourceAllocation autoAllocate(Long requestId) {
+        // dummy implementation for now
+        return new ResourceAllocation();
+    }
+
+    @Override
+    public ResourceAllocation getAllocation(Long id) {
+        // dummy implementation for now
+        return new ResourceAllocation();
+    }
+
+    @Override
+    public List<ResourceAllocation> getAllAllocations() {
+        return new ArrayList<>();
+    }
 }
