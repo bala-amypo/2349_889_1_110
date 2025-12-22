08fe1e9f813576806159e2c59615ef1c829d437e
bala-amypo <bala@amypo.in>
2025-12-22 07:48:48 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -33,7 +33,7 @@ public class User {
         this.createdAt = LocalDateTime.now();
     }
 
-    // Getters and setters
+    
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
