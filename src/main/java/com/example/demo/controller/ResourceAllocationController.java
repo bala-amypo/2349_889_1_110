57883e588276885cdaa9b34f2024fdfc22fea295
bala-amypo <bala@amypo.in>
2025-12-25 09:04:22 +0000

    User commit

--- src/main/java/com/example/demo/controller/ResourceAllocationController.java
+++ src/main/java/com/example/demo/controller/ResourceAllocationController.java
@@ -1,23 +1,13 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.ResourceAllocation;
-import com.example.demo.service.ResourceAllocationService;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/allocations")
 public class ResourceAllocationController {
 
-    private final ResourceAllocationService allocationService;
-
-    public ResourceAllocationController(ResourceAllocationService allocationService) {
-        this.allocationService = allocationService;
-    }
-
     @GetMapping
-    public List<ResourceAllocation> getAllAllocations() {
-        return allocationService.getAllAllocations();
+    public String allocationsInfo() {
+        return "Resource Allocation API";
     }
 }
