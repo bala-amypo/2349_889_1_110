8d215a96a3ccec25168322cae42bcfd30b1f86a4
bala-amypo <bala@amypo.in>
2025-12-25 09:53:13 +0000

    User commit

--- src/main/java/com/example/demo/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String msg) {
-        super(msg);
-    }
-}
--- src/main/java/com/example/demo/repository/ResourceAllocationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.*;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-
-public interface ResourceAllocationRepository extends JpaRepository<ResourceAllocation, Long> {}
