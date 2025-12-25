44aebf0d3e2ba83008415080b5b22f90e621abda
bala-amypo <bala@amypo.in>
2025-12-25 09:16:21 +0000

    User commit

--- src/main/java/com/example/demo/controller/AllocationRuleController.java
+++ src/main/java/com/example/demo/controller/AllocationRuleController.java
@@ -1,13 +1,28 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.AllocationRule;
+import com.example.demo.service.AllocationRuleService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/allocation-rules")
+@RequestMapping("/rules")
+@Tag(name = "Allocation Rule")
 public class AllocationRuleController {
 
-    @GetMapping
-    public String allocationRulesInfo() {
-        return "Allocation Rule API";
+    private final AllocationRuleService ruleService;
+
+    public AllocationRuleController(AllocationRuleService ruleService) {
+        this.ruleService = ruleService;
+    }
+
+    @PostMapping
+    public AllocationRule create(@RequestBody AllocationRule rule) {
+        return ruleService.createRule(rule);
+    }
+
+    @GetMapping("/{name}")
+    public AllocationRule get(@PathVariable String name) {
+        return ruleService.getRule(name);
     }
 }
