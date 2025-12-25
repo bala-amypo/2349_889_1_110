5811e480c153ac97add68171e66ba5d316d04643
bala-amypo <bala@amypo.in>
2025-12-25 11:17:30 +0000

    User commit

--- src/main/java/com/example/demo/repository/ResourceAllocationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.ResourceAllocation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResourceAllocationRepository extends JpaRepository<ResourceAllocation, Long> {
-}
