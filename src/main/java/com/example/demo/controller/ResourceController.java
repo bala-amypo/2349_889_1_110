934a4032e062109325c426c62725e58d848565fa
bala-amypo <bala@amypo.in>
2025-12-25 09:13:22 +0000

    User commit

--- src/main/java/com/example/demo/controller/ResourceController.java
+++ src/main/java/com/example/demo/controller/ResourceController.java
@@ -1,13 +1,35 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Resource;
+import com.example.demo.service.ResourceService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/resources")
+@Tag(name = "Resource")
 public class ResourceController {
 
+    private final ResourceService resourceService;
+
+    public ResourceController(ResourceService resourceService) {
+        this.resourceService = resourceService;
+    }
+
+    @PostMapping
+    public Resource create(@RequestBody Resource resource) {
+        return resourceService.createResource(resource);
+    }
+
     @GetMapping
-    public String resourcesInfo() {
-        return "Resource API";
+    public List<Resource> getAll() {
+        return resourceService.getAllResources();
+    }
+
+    @GetMapping("/type/{type}")
+    public List<Resource> findByType(@PathVariable String type) {
+        return resourceService.findByType(type);
     }
 }
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,13 +1,28 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
+@Tag(name = "User")
 public class UserController {
 
-    @GetMapping
-    public String usersInfo() {
-        return "User API";
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+
+    @GetMapping("/{email}")
+    public User getByEmail(@PathVariable String email) {
+        return userService.findByEmail(email);
     }
 }
