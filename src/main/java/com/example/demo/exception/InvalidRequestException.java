7e979cc0d682073d659164ac6f1ece81c3c373e5
bala-amypo <bala@amypo.in>
2025-12-25 12:15:55 +0000

    User commit

--- src/main/java/com/example/demo/exception/InvalidRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.exception;
-
-public class InvalidRequestException extends RuntimeException {
-    public InvalidRequestException(String message) {
-        super(message);
-    }
-}
